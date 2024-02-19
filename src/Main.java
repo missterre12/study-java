a7eba528ebc3a3ac64c9e8e8ae8d30a7c06a93c1
missterre12 <terresasbusiness@gmail.com>
2024-02-19 17:46:13 +0700

    initial commit

---
+++ src/Main.java
@@ -0,0 +1,21 @@
+public class Main {
+    public static void main(String[] args) {
+        // Doesn't insert a new line
+        System.out.print("Java is awesome!");
+        // I add an extra space in front of the sentence for better readability
+        System.out.print(" That's why I like Java!");
+
+        // Insert a new line
+        System.out.println("Hello World!");
+        System.out.println(100);
+        System.out.println(5+5);
+
+        String name = "John";
+        System.out.println(name);
+
+        int myNum = 15;
+        System.out.println(myNum);
+
+        // It's declare the variable as "final" or "constant" such as final int = 5, which means unchangeable and read-only
+    }
+}

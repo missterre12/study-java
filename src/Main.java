125538b3b46631fc7ef508fb3ace075b332e3256
missterre12 <terresasbusiness@gmail.com>
2024-02-20 08:45:52 +0700

    add folder assets and discuss about if statements

--- src/Main.java
+++ src/Main.java
@@ -17,5 +17,67 @@ public class Main {
         System.out.println(myNum);
 
         // It's declare the variable as "final" or "constant" such as final int = 5, which means unchangeable and read-only
+
+        int time = 22;
+        if (time < 10) {
+            System.out.println("Good Morning!");
+        } else if (time < 18) {
+            System.out.println("Good Afternoon!");
+        } else {
+            System.out.println("Good Evening!");
+        }
+
+        // Simple Write
+        int times = 20;
+        String result = (times < 18) ? "Good Afternoon!" : "Good Evening!";
+        System.out.println(result);
+
+        // Use switch case
+        int day = 5;
+        switch (day) {
+            case 1:
+                System.out.println("Monday");
+                break;
+            case 2:
+                System.out.println("Tuesday");
+                break;
+            case 3:
+                System.out.println("Wednesday");
+                break;
+            case 4:
+                System.out.println("Thursday");
+                break;
+            case 5:
+                System.out.println("Friday");
+                break;
+            case 6:
+                System.out.println("Saturday");
+                break;
+            case 7:
+                System.out.println("Sunday");
+                break;
+            default:
+                System.out.println("Looking for what day?");
+        }
+
+        // Print even multiple number of 2
+        for (int i = 0; i <= 10; i = i + 2) {
+            System.out.println(i);
+        }
+
+        // Print even multiple number of 2 under 10
+        for (int j = 0; j < 10; j = j + 2) {
+            System.out.println(j);
+        }
+
+        // Outer loop
+        for (int k = 1; k <= 2; k++) {
+            System.out.println("Outer: " + k); // Executes 2 times
+
+            // Inner loop
+            for (int m = 1; m <= 3; m++) {
+                System.out.println(" Inner: " + m); // Executes 6 times (2 * 3)
+            }
+        }
     }
 }
